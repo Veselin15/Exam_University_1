2d53e8da978f2deb78460253b86c224ef5931e6e
Veselin <veskoveskov2006@gmail.com>
2025-10-29 20:11:59 +0200

    Initial

---
+++ Exam_University_1.cpp
@@ -0,0 +1,12 @@
+﻿// Exam_University_1.cpp : Defines the entry point for the application.
+//
+
+#include "Exam_University_1.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
